e3d94b0eb07e524f8e256372095f48757d250ca4
Melissa Benard <benardmeli@yahoo.fr>
2022-12-08 12:32:17 +0100

    update a flower

--- src/Controller/AdminFlowerController.php
+++ src/Controller/AdminFlowerController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Flower;
+use App\Form\FlowerType;
 use App\Repository\FlowerRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class AdminFlowerController extends AbstractController
 {
+    /**
+     * Get all the flowers
+     *
+     * @param FlowerRepository $flowerRepository
+     * @return Response
+     */
     #[Route('/admin/flower', name: 'app_admin_flower')]
     public function getAll(FlowerRepository $flowerRepository): Response
     {
@@ -18,4 +29,28 @@ class AdminFlowerController extends AbstractController
             "flowers" => $flowers
         ]);
     }
+
+    /**
+     * Update a flower
+     *
+     * @param Flower $flower
+     * @return Response
+     */
+    #[Route('/admin/flower/{id}', name: 'app_admin_update_flower')]
+    public function update(Flower $flower, Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $form = $this->createForm(FlowerType::class, $flower);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $managerRegistry->getManager()->persist($flower);
+            $managerRegistry->getManager()->flush();
+            return $this->redirectToRoute('app_admin_flower');
+        }
+
+        return $this->render('admin_flower/update.html.twig', [
+            "flower" => $flower,
+            "form" => $form->createView()
+        ]);
+    }
+
 }
---
+++ src/Form/FlowerType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Discount;
+use App\Entity\Flower;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FlowerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('price', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('isNew')
+            ->add('image', TextType::class)
+            ->add('discount', EntityType::class, [
+                'class' => Discount::class,
+                'choice_label' => 'value'
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Flower::class,
+
+        ]);
+    }
+}
